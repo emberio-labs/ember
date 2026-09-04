887c9fc7c4a4a64e5cd61a29ded50e21eba244d7
Alexander Sapegin <sapegin04@mail.ru>
2026-09-04 23:09:34 +0500

    chore: bump version to 0.3.0

--- ember/__init__.py
+++ ember/__init__.py
@@ -21,7 +21,7 @@ from ember.types import (
     Usage,
 )
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "__version__",
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,4 +2,4 @@ import ember
 
 
 def test_version() -> None:
-    assert ember.__version__ == "0.2.0"
+    assert ember.__version__ == "0.3.0"
